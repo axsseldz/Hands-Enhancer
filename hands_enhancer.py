fb076822d21bd75e4b308d2d48b3e9657f2fda79
axel <axssel.diazfl@gmail.com>
2024-08-15 11:14:58 -0700

    update

--- hands_enhancer.py
+++ hands_enhancer.py
@@ -1,23 +1,25 @@
-import requests
+import websocket  # NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
 import uuid
 import json
-import os
+import urllib.request
 import urllib.parse
 from PIL import Image
 import io
-import websocket
+import requests
+import os
+from pathlib import Path
 
-server_address = "rs2k5p4qmzgxin-3020.proxy.runpod.net"
-
+server_address = "URL"
 client_id = str(uuid.uuid4())
 
-def queue_prompt(prompt, files):
-    payload = {"workflow_id": client_id, "prompt": prompt, "files": files}
-    data = json.dumps(payload)
-    headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0'}
-    url = f"https://{server_address}/prompt"
-    print(f"Sending POST request to {url} with payload:\n{json.dumps(payload, indent=2)}")
+
+headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0'}
+
+def queue_prompt(prompt):
+    p = {"prompt": prompt, "client_id": client_id}
+    data = json.dumps(p).encode('utf-8')
     
+    url = f"https://{server_address}/prompt"
     response = requests.post(url, data=data, headers=headers)
     if response.status_code != 200:
         print(f"HTTP Error: {response.status_code} - {response.reason}")
@@ -28,104 +30,71 @@ def queue_prompt(prompt, files):
 def get_image(filename, subfolder, folder_type):
     data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
     url_values = urllib.parse.urlencode(data)
-    headers = {'User-Agent': 'Mozilla/5.0'}
     url = f"https://{server_address}/view?{url_values}"
+    
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.content
 
 def get_history(prompt_id):
-    headers = {'User-Agent': 'Mozilla/5.0'}
     url = f"https://{server_address}/history/{prompt_id}"
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
 
-def connect_to_websocket():
-    ws = websocket.WebSocket()
-    try:
-        ws.connect(f"wss://{server_address}/ws?clientId={client_id}")
-        return ws
-    except websocket.WebSocketException as e:
-        print(f"WebSocket connection error: {e}")
-        return None
-
-def get_images(ws, prompt, files):
-    prompt_id = queue_prompt(prompt, files).get('prompt_id')
-    if not prompt_id:
-        print("Failed to get prompt_id.")
-        return {}
-
+def get_images(ws, prompt):
+    prompt_id = queue_prompt(prompt)['prompt_id']
     output_images = {}
     while True:
-        try:
-            out = ws.recv()
-            print(f"Received WebSocket message: {out}")
-            if isinstance(out, str):
-                message = json.loads(out)
-                if message['type'] == 'executing':
-                    data = message['data']
-                    if data['node'] is None and data['prompt_id'] == prompt_id:
-                        print("Execution complete.")
-                        break  
-            else:
-                continue
-        except Exception as e:
-            print(f"Error receiving WebSocket message: {e}")
-            break
+        out = ws.recv()
+        if isinstance(out, str):
+            message = json.loads(out)
+            if message['type'] == 'executing':
+                data = message['data']
+                if data['node'] is None and data['prompt_id'] == prompt_id:
+                    break  
+        else:
+            continue 
 
-    history = get_history(prompt_id)
-    print(f"History data: {json.dumps(history, indent=2)}")
-    if prompt_id not in history:
-        print(f"Prompt ID {prompt_id} not found in history.")
-        return {}
-
-    history_data = history[prompt_id]
-    for node_id in history_data['outputs']:
-        node_output = history_data['outputs'][node_id]
-        if 'images' in node_output:
-            images_output = []
-            for image in node_output['images']:
-                image_data = get_image(image['filename'], image['subfolder'], image['type'])
-                images_output.append(image_data)
+    history = get_history(prompt_id)[prompt_id]
+    for o in history['outputs']:
+        for node_id in history['outputs']:
+            node_output = history['outputs'][node_id]
+            if 'images' in node_output:
+                images_output = []
+                for image in node_output['images']:
+                    image_data = get_image(image['filename'], image['subfolder'], image['type'])
+                    images_output.append(image_data)
             output_images[node_id] = images_output
 
     return output_images
 
+with open("hands_wf.json", "r", encoding="utf-8") as f:
+    workflow_jsondata = f.read()
 
-with open("workflow.json", "r", encoding="utf-8") as f:
-    workflow_data = f.read()
+jsonwf = json.loads(workflow_jsondata)
 
-prompt = json.loads(workflow_data)
+jsonwf["32"]["inputs"]["text"] = "<lora:austin_reaves_v0.1:0.9> tom holland (looking at the camera:1.3), <lora:add-detail-xl:2>, <lora:cinematic-lighting:2> cinematic lighting, (high-resolution background:1.2), full body, showing the thumbs up, wearing a white shirt and blue jeans, sitting in a bench at the beach, natural light, (real-life moment:1.3), (Fujifilm X-T4, 1/125s, f/4, ISO 800), (natural pose:1.3), masterpiece, high resolution, skin imperfections, photorealistic, DSLR, depth of field, (perfect eyes:1.3)"
+jsonwf["31"]["inputs"]["seed"] = 6
 
-image_path = "./images/image.png"
+ws_protocol = "wss"  
+ws_url = "{}://{}/ws?clientId={}".format(ws_protocol, server_address, client_id)
 
-files = {
-    "/input/image.png": image_path,
-}
 
-prompt["18"]["inputs"]["image"] = "image.png"
-prompt["32"]["inputs"]["text"] = "<lora:austin_reaves_v0.1:0.9> tom holland (looking at the camera:1.3), <lora:add-detail-xl:2>, <lora:cinematic-lighting:2> cinematic lighting, (high-resolution background:1.2), full body, showing the thumbs up, wearing a white shirt and blue jeans, sitting in a bench at the beach, natural light, (real-life moment:1.3), (Fujifilm X-T4, 1/125s, f/4, ISO 800), (natural pose:1.3), masterpiece, high resolution, skin imperfections, photorealistic, DSLR, depth of field, (perfect eyes:1.3)"
+print("Connecting to WebSocket URL:", ws_url)
 
-def process_images():
-    ws = connect_to_websocket()
-    if ws is None:
-        print("Failed to connect to WebSocket.")
-        return
+ws = websocket.WebSocket()
+ws.connect(ws_url)  
 
-    try:
-        images = get_images(ws, prompt, files)
-    finally:
-        ws.close()
+images = get_images(ws, jsonwf)
 
-    os.makedirs("./images/output_imgs", exist_ok=True)
-    image_counter = 1
-    for node_id in images:
-        for image_data in images[node_id]:
-            image = Image.open(io.BytesIO(image_data))
-            image_path = f"./images/output_imgs/output_image_{image_counter}.png"
-            image.save(image_path)
-            print(f"Saved image {image_counter} to {image_path}")
-            image_counter += 1
 
-process_images()
+os.makedirs("./output_imgs", exist_ok=True)
+image_counter = 1
+for node_id in images:
+    for image_data in images[node_id]:
+        image = Image.open(io.BytesIO(image_data))
+        image_path = f"./output_imgs/image_{image_counter}.png"
+        image.save(image_path)
+        print(f"Saved image {image_counter} to {image_path}")
+        image_counter += 1
